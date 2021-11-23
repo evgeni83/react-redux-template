d4c881962ac7c86f9cd3846f04a971f31972d99f
evgeni83 <evgeni.daniluk@yandex.ru>
2021-11-23 11:26:28 +0300

    added plugins for combining css media-queries and css rules

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-	plugins: ["postcss-preset-env"],
+	sourcemap: true,
+	plugins: [
+		require( 'postcss-combine-media-query' ),
+		require( 'cssnano' )( {
+			preset: 'default',
+		} ),
+		require( 'postcss-preset-env' ),
+	],
 };
